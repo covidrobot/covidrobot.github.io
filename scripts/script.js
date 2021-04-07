3b98fdd5a118184f5f6a0071d311d15c8897c435
covidrobot <79981007+covidrobot@users.noreply.github.com>
2021-04-07 13:44:24 +0200

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+import { countries } from "covidapi";
+
+setInterval(() => {
+
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+const data = countries({country: 'slovakia'}).then(console.log)
+   window.document.getElementById("casesOverall").innerHTML = "test"
+}, 1000)
+    
